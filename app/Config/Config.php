73e99a5a850b44ce98ff9148ba2c100a9d95e72d
YQP PRODUCCIONES <yordicito2014@gmail.com>
2021-06-15 22:27:42 -0500

    version 1.0

---
+++ app/Config/Config.php
@@ -0,0 +1,26 @@
+<?php
+//const SiteUrl ="https://knetsistemas.com/";
+//const ApiUrl = "https://clientes.shirlasoft.store/";
+const SiteUrl ="http://localhost/knetsistemas/";
+const ApiUrl = "http://localhost/admin/";
+const SiteName = "Universal";
+
+//rutas de carpetas de la aplicacion
+const DirBack = "../";
+const DirApp = DirBack."app/";
+const DirControllers = "Controllers/";
+const DirHelpers = "Helpers/";
+const DirModels = "Models/";
+const DirViews = "Views/";
+const DirLibrary = "Library/";
+const DirImg= "img/";
+const DirLayout= DirApp."Views/Layout/";
+const php = ".php";
+const html = ".html";
+
+/* headers */
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: *");
+header('Access-Control-Allow-Methods: *');
+
+?>
